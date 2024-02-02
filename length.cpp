94b927273c730bb4da9c1a3a049c6880a5f23d2d
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-02-02 20:04:50 +0600

    Add files via upload

---
+++ length.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char s1[30];
+    cout<<"inter your name:";
+    gets(s1);
+  int str =strlen(s1);
+  cout<<"s1="<<s1<<endl;
+   cout<<"s1="<<str;
+
+
+
+    getch();
+}
