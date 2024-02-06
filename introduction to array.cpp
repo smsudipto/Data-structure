2909df3f14653fa9bc8b0c0f01ac7bc60ff71a8f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-02-07 00:13:45 +0600

    Add files via upload

---
+++ introduction to array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char s1[9];
+    s1[0]='s';
+    s1[1]='u';
+    s1[2]='d';
+    s1[3]='i';
+    s1[4]='p';
+    s1[5]='t';
+    s1[6]='o';
+    s1[7]='\0';
+    cout<<"s1[4]="<<s1[4]<<endl;
+    cout<<"s1="<<s1;;
+    getch();
+}
